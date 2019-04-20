e34130bb7c9381c344205f97c431d93a320e67e2
josephguan <guanxun555@gmail.com>
2019-04-20 23:28:02 +0800

    join dimension table using temporal table function

--- flink/src/main/scala/gx/flink/table/JoinTableStaticExample.scala
+++ flink/src/main/scala/gx/flink/table/JoinTableStaticExample.scala
@@ -6,6 +6,7 @@ import java.util.Date
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.table.api.Types
 import org.apache.flink.table.api.scala._
 
 
@@ -25,30 +26,39 @@ object JoinTableStaticExample {
     // set up tables
     val now = new Date().getTime
 
-    val orderA = env.fromCollection(Seq(
-      Order(1L, "beer", 3, new Timestamp(now)),
-      Order(1L, "diaper", 4, new Timestamp(now + 10000)),
-      Order(3L, "apple", 5, new Timestamp(now + 20000)),
-      Order(3L, "apple", 6, new Timestamp(now + 30000)),
-      Order(2L, "apple", 7, new Timestamp(now + 40000)),
-      Order(2L, "apple", 6, new Timestamp(now + 20000)),
-      Order(3L, "rubber", 2, new Timestamp(now + 30000))))
-      .assignAscendingTimestamps(_.time.getTime)
-      .toTable(tEnv, 'user, 'product, 'amount, 'time.rowtime)
 
-    val userInfo = env.fromCollection(Seq(
+    val userInfoTable = env.fromCollection(Seq(
       UserInfo(1L, "andy", 10),
       UserInfo(2L, "bobby", 15),
       UserInfo(3L, "catty", 20)))
-      .toTable(tEnv, 'id, 'name, 'age)
+      .toTable(tEnv, 'id, 'name, 'age, 'r_proctime.proctime)
 
-    // join the two tables with 5 seconds time window
-    val result: DataStream[Result] = orderA.leftOuterJoin(userInfo)
-      .where('user==='id)
-      .select('user, 'product, 'amount, 'name, 'age, 'time)
+    val orderA = env.fromCollection(Seq(
+      Order(1L, "beer", 3, new Timestamp(now - 10000)),
+      Order(1L, "diaper", 4, new Timestamp(now - 10000)),
+      Order(3L, "apple", 5, new Timestamp(now - 20000)),
+      Order(3L, "apple", 6, new Timestamp(now - 30000)),
+      Order(2L, "apple", 7, new Timestamp(now - 40000)),
+      Order(2L, "apple", 6, new Timestamp(now - 20000)),
+      Order(3L, "rubber", 2, new Timestamp(now - 30000))))
+      .assignAscendingTimestamps(_.time.getTime)
+      .toTable(tEnv, 'user, 'product, 'amount, 'time.rowtime, 'o_proctime.proctime)
+
+
+    // create temporal table function
+    val userInfo = userInfoTable
+      .createTemporalTableFunction('r_proctime, 'id)
+
+    // join with temporal table, which can be treated as dimension table
+    val result: DataStream[Result] = orderA.joinLateral(userInfo('o_proctime), 'user === 'id)
+      .select('user, 'product, 'amount, 'name, 'age, 'time, 'o_proctime, 'r_proctime)
       .toAppendStream[Result]
 
     result.print()
+
+    // force printing orderA table to make its proctime a little bit latter than userInfoTable
+    orderA.toAppendStream[OrderTimed].print()
+    userInfoTable.toAppendStream[UserInfoTimed].print()
 
     env.execute()
   }
@@ -58,7 +68,14 @@ object JoinTableStaticExample {
   // *************************************************************************
 
   case class Order(user: Long, product: String, amount: Int, time: Timestamp)
+
+  case class OrderTimed(user: Long, product: String, amount: Int, time: Timestamp, pt: Timestamp)
+
   case class UserInfo(id: Long, name: String, age: Int)
-  case class Result(user: Long, product: String, amount: Int, name: String, age: Int, time: Timestamp)
+
+  case class UserInfoTimed(id: Long, name: String, age: Int, pt: Timestamp)
+
+  case class Result(user: Long, product: String, amount: Int, name: String, age: Int, time: Timestamp,
+                    o_proctime: Timestamp, r_proctime: Timestamp)
 
 }
